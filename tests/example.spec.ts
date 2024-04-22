cf2a0e7e626ac1fe106f023a4ecd25f7218f6c14
trieu_quan <trieu_quan@fastboy.net>
2024-04-22 11:38:40 +0700

    change test get text

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -7,14 +7,9 @@ test("has title", async ({ page }) => {
   await expect(page).toHaveTitle(/React/);
 });
 
-// test("get started link", async ({ page }) => {
-//   await page.goto("https://playwright.dev/");
+test("get Text", async ({ page }) => {
+  await page.goto("https://trieuquan27.github.io/");
 
-//   // Click the get started link.
-//   await page.getByRole("link", { name: "Get started" }).click();
-
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(
-//     page.getByRole("heading", { name: "Installation" })
-//   ).toBeVisible();
-// });
+  // Click the get started link.
+  await expect(page.getByText("My Tsk")).toBeVisible();
+});
