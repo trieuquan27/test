81130373f87cce2d4feea3a4aa0259b085a8bcb1
Trieu Quan <116069686+trieuquan27@users.noreply.github.com>
2024-05-04 01:19:11 +0700

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test("has title", async ({ page }) => {
-  await page.goto("https://trieuquan27.github.io/");
+  await page.goto("https://trieuquan.pages.dev/");
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/React/);
